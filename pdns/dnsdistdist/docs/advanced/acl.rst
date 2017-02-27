5cce4cda94caf9ed9dd182f4d22c5ea02a64c112
Pieter Lexis <pieter.lexis@powerdns.com>
2017-02-27 21:09:33 +0100

    Document the API

--- pdns/dnsdistdist/docs/advanced/acl.rst
+++ pdns/dnsdistdist/docs/advanced/acl.rst
@@ -1,3 +1,5 @@
+.. _ACL:
+
 Access Control
 ==============
 
--- pdns/dnsdistdist/docs/conf.py
+++ pdns/dnsdistdist/docs/conf.py
@@ -30,7 +30,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['redjack.sphinx.lua']
+extensions = ['redjack.sphinx.lua', 'sphinxcontrib.httpdomain', 'sphinxjsondomain']
 primary_domain = 'lua'
 
 # Add any paths that contain templates here, relative to this directory.
